40c866ac10219ec688989192b43d092486badc3f
Jared Abbott <jaredabbott@bluedrop.com>
2017-02-19 00:03:31 -0330

    Refactor

--- task/style.compile.js
+++ task/style.compile.js
@@ -6,12 +6,12 @@ const finder = require("glob-concat");
 
 const tmpCSS = "tmp/formulus.tmp.scss";
 const inputSCSS = "src/formulus.scss";
-const outputCSS = "dist/formulus.min.css";
+const outputCSS = "dist/css/formulus.min.css";
 const postCSS = "chmod +x node_modules/postcss/lib/postcss.js && node node_modules/postcss/lib/postcss.js";
 const nodeSASS = "chmod +x node_modules/node-sass/bin/node-sass && node_modules/node-sass/bin/node-sass";
 
-sh.exec("mkdir ./dist/");
-sh.exec("mkdir ./dist/fonts/");
+sh.exec("mkdir ./dist/fonts");
+sh.exec("mkdir ./dist/css/");
 sh.exec("mkdir ./tmp/");
 sh.exec("touch " + tmpCSS);
 sh.exec("touch " + outputCSS);
@@ -20,6 +20,7 @@ sh.exec("touch " + outputCSS);
 sh.exec("node task/font.copy.js");
 
 sh.exec("cp " + inputSCSS + " " + tmpCSS);
-sh.exec(nodeSASS + " -q --output-style compressed --include-path scss " + inputSCSS + " > " + tmpCSS);
+sh.exec(nodeSASS + " -q --output-style compressed --include-path scss " + inputSCSS + " " + tmpCSS);
 sh.exec(postCSS + " --use autoprefixer -b 'last 5 versions' < " + tmpCSS);
-sh.exec("cp " + tmpCSS + " " + outputCSS);
+
+sh.exec("cat node_modules/font-awesome/css/font-awesome.min.css " + tmpCSS + " >> " + outputCSS);
